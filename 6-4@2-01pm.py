d996eceecec777bc31c933a23bfbcba4c42f16c8
avdnn8 <nadaduraravind@gmail.com>
2024-04-06 15:09:00 +1100

    -

--- 6-4@2-01pm.py
+++ 6-4@2-01pm.py
@@ -11,17 +11,17 @@ class Graph:
             self.graph[vertex1].append(vertex2)
             self.graph[vertex2].append(vertex1)
 
-    def remove_vertex(self, vertex):
+    def delete_vertex(self, vertex):
         """
-        Remove a vertex from the graph.
+        Delete a vertex from the graph.
 
-        :param vertex: The vertex to be removed
+        :param vertex: The vertex to be deleted
         """
         if vertex in self.graph:
             del self.graph[vertex]
-            for v in self.graph:
-                if vertex in self.graph[v]:
-                    self.graph[v].remove(vertex)
+            for adjacent_vertices in self.graph.values():
+                if vertex in adjacent_vertices:
+                    adjacent_vertices.remove(vertex)
 
     def remove_edge(self, vertex1, vertex2):
         """
@@ -36,24 +36,18 @@ class Graph:
             if vertex1 in self.graph[vertex2]:
                 self.graph[vertex2].remove(vertex1)
 
-    def get_friends(self, vertex):
-        """
-        Get the friends (adjacent nodes) of a vertex.
-
-        :param vertex: The vertex to get friends for
-        :return: A list of friends
-        """
-        if vertex in self.graph:
-            return self.graph[vertex]
-        else:
-            return []
-
     def display_graph(self):
-        """
-        Print the graph.
-        """
         for vertex in self.graph:
             print(vertex, "->", ', '.join(self.graph[vertex]))
+
+    def show_friends(self, vertex):
+        """
+        Show the friends (adjacent nodes) of a vertex.
+
+        :param vertex: The vertex whose friends you want to show
+        """
+        if vertex in self.graph:
+            print("Friends of", vertex, ":", ', '.join(self.graph[vertex]))
 
 
 # Example usage
@@ -65,8 +59,8 @@ g.add_edge("A", "B")
 g.add_edge("B", "C")
 g.add_edge("A", "C")
 g.display_graph()
-print(g.get_friends("A"))
+g.show_friends("A")
 g.remove_edge("A", "B")
 g.display_graph()
-g.remove_vertex("B")
+g.delete_vertex("C")
 g.display_graph()
