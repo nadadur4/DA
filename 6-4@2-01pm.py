25e074da50da5d722c5acd90261835e4048f4671
avdnn8 <nadaduraravind@gmail.com>
2024-04-06 14:44:37 +1100

    A

---
+++ 6-4@2-01pm.py
@@ -0,0 +1,42 @@
+class Graph:
+    def __init__(self):
+        # Initialize an empty dictionary to store the graph
+        self.graph = {}
+
+    def add_vertex(self, vertex):
+        """
+        Add a new vertex to the graph.
+
+        :param vertex: The vertex to be added
+        """
+        if vertex not in self.graph:
+            self.graph[vertex] = []
+
+    def add_edge(self, vertex1, vertex2):
+        """
+        Add a new edge between two vertices.
+
+        :param vertex1: The first vertex of the edge
+        :param vertex2: The second vertex of the edge
+        """
+        if vertex1 in self.graph and vertex2 in self.graph:
+            self.graph[vertex1].append(vertex2)
+            self.graph[vertex2].append(vertex1)
+
+    def display_graph(self):
+        """
+        Print the graph.
+        """
+        for vertex in self.graph:
+            print(f"{vertex} -> {self.graph[vertex]}".replace("\'", ""))
+
+
+# Example usage
+g = Graph()
+g.add_vertex("A")
+g.add_vertex("B")
+g.add_vertex("C")
+g.add_edge("A", "B")
+g.add_edge("B", "C")
+g.add_edge("A", "C")
+g.display_graph()
